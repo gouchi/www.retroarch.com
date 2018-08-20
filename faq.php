f66cec2f95db4d9de26d53d4e901fbffeb8f0bd1
Twinaphex <libretro@gmail.com>
2018-08-20 15:45:40 +0200

    Create faq.php

---
+++ faq.php
@@ -0,0 +1,21 @@
+ <?php $version = "1.7.3"; ?>
+
+<div id="welcome">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 abstract">
+                <h1>Frequenty Asked Questions</h1>
+                <p>TODO</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="doc">
+    <div class="container">
+        <h2>Which core is the best to emulate (some console / game) ?</h2>
+        <div class="row platforms">
+        <p>There is no best core, each has a different use case (accuracy, speed...).</p>.
+        </div>
+    </div>
+</div>
